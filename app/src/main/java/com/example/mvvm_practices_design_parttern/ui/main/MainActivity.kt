9bc5c0d76985e9b9939ea37d9d4dee2c9c057aca
Sokheng Chhoeurn <chhoeurnsokheng@gmail.com>
2021-07-06 16:58:51 +0700

    [UNTRACKED]-Add Support with mode

--- app/src/main/java/com/example/mvvm_practices_design_parttern/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_practices_design_parttern/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.mvvm_practices_design_parttern.ui.main
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -13,9 +17,7 @@ import com.example.mvvm_practices_design_parttern.R
 import com.example.mvvm_practices_design_parttern.databinding.ActivityMainBinding
 import com.example.mvvm_practices_design_parttern.ui.profile.UserProfileActivity
 import com.example.mvvm_practices_design_parttern.ui.uploadpicture.UploadProfileActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationView.OnNavigationItemSelectedListener {
@@ -23,13 +25,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationView.OnNavig
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+       // setSupportActionBar(toolbar)
+        setupNavigation()
+        checkTheme()
+        switch_darkLight.setOnClickListener { chooseThemeDialog()
         }
+    }
+
+    fun setupNavigation(){
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val toggle = ActionBarDrawerToggle(
@@ -41,7 +44,53 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationView.OnNavig
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
         imageSliderImplementation()
+    }
+    private fun chooseThemeDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.choose_theme_text))
+        val styles = arrayOf("Light", "Dark", "System default")
+        val checkedItem = MyPreferences(this).darkMode
+        builder.setSingleChoiceItems(styles, checkedItem) { dialog, which ->
+            when (which) {
+                0 -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    MyPreferences(this).darkMode = 0
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+                1 -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    MyPreferences(this).darkMode = 1
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+                2 -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    MyPreferences(this).darkMode = 2
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
 
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+    private fun checkTheme() {
+        when (MyPreferences(this).darkMode) {
+            0 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                delegate.applyDayNight()
+            }
+            1 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                delegate.applyDayNight()
+            }
+            2 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                delegate.applyDayNight()
+            }
+        }
     }
 
     private fun imageSliderImplementation() {
@@ -58,14 +107,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationView.OnNavig
         }
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
@@ -73,7 +118,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationView.OnNavig
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_home -> {
                 startActivity(Intent(this,UserProfileActivity::class.java))
@@ -93,6 +137,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationView.OnNavig
             R.id.nav_send -> {
 
             }
+            R.id.nav_dark->{
+
+            }
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         drawerLayout.closeDrawer(GravityCompat.START)
@@ -102,4 +149,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationView.OnNavig
     override fun getContentLayoutId(): Int {
         return  R.layout.activity_main
     }
+}
+class MyPreferences(context: Context?) {
+    companion object {
+        private const val DARK_STATUS = "io.github.manuelernesto.DARK_STATUS"
+    }
+    private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+    var darkMode = preferences.getInt(DARK_STATUS, 0)
+        set(value) = preferences.edit().putInt(DARK_STATUS, value).apply()
+
 }
--- app/src/main/java/com/example/mvvm_practices_design_parttern/ui/uploadpicture/UploadProfileActivity.kt
+++ app/src/main/java/com/example/mvvm_practices_design_parttern/ui/uploadpicture/UploadProfileActivity.kt
@@ -1,108 +1,67 @@
 package com.example.mvvm_practices_design_parttern.ui.uploadpicture
 
 import android.Manifest
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import com.example.mvvm_practices_design_parttern.R
-import kotlinx.android.synthetic.main.activity_upload_profile.*
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
 
 class UploadProfileActivity : AppCompatActivity() {
-    var photoFile: File? = null
-    val CAPTURE_IMAGE_REQUEST = 1
-    var mCurrentPhotoPath: String? = null
-
+    private lateinit var btnTakePhoto: Button
+    var imageView: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload_profile)
+        btnTakePhoto = findViewById(R.id.button)
+        imageView = findViewById(R.id.imageView)
+        EnableRuntimePermission()
 
-        button_capture.setOnClickListener {
-            captureImage()
-        }
+        btnTakePhoto.setOnClickListener(View.OnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 7)
+        })
 
-    }
-
-    private fun captureImage() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
-        } else {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            if (takePictureIntent.resolveActivity(packageManager) != null) {
-                // Create the File where the photo should go
-                try {
-                    photoFile = createImageFile()
-                    // Continue only if the File was successfully created
-                    if (photoFile != null) {
-                        val photoURI = FileProvider.getUriForFile(
-                            this,
-                            "com.example.captureimage.fileprovider",
-                            photoFile!!
-                        )
-                        takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST)
-                    }
-                } catch (ex: Exception) {
-                    // Error occurred while creating the File
-                    displayMessage(baseContext, ex.message.toString())
-                }
-
-            } else {
-                displayMessage(baseContext, "Null")
-            }
-        }
-
-    }
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val imageFileName = "JPEG_" + timeStamp + "_"
-        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val image = File.createTempFile(
-            imageFileName, /* prefix */
-            ".jpg", /* suffix */
-            storageDir      /* directory */
-        )
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.absolutePath
-        return image
-    }
-
-    private fun displayMessage(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            val myBitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath)
-            imageView.setImageBitmap(myBitmap)
-        } else {
-            displayMessage(baseContext, "Request cancelled or something went wrong.")
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+            val bitmap = data!!.extras!!["data"] as Bitmap?
+            imageView!!.setImageBitmap(bitmap)
         }
     }
 
+    fun EnableRuntimePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Toast.makeText(this, "CAMERA permission allows us to Access CAMERA app", Toast.LENGTH_LONG).show()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), RequestPermissionCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        result: IntArray
+    ) {
+        when (requestCode) {
+            RequestPermissionCode -> if (result.size > 0 && result[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission Canceled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    companion object {
+        const val RequestPermissionCode = 1
+    }
 }
